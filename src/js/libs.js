dabd11745f77ae5ee7bef290b602016209072b59
Will IM <vitalii.mieliet@gmail.com>
2025-05-07 23:59:45 +0300

    Add dark theme flatpicr

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,5 +1,5 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/dark.css';
 import confirmDatePlugin from 'flatpickr/dist/plugins/confirmDate/confirmDate.js';
 import 'flatpickr/dist/plugins/confirmDate/confirmDate.css';
 import iziToast from 'izitoast';
