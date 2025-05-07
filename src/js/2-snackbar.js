ecef3f2774a2056b9e75ba9f38b00b366268fbc2
Will IM <vitalii.mieliet@gmail.com>
2025-05-08 00:12:35 +0300

    moved promise creation to a separate function

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,22 +1,30 @@
-/*
-Напиши скрипт, який після сабміту форми створює проміс. В середині колбека цього промісу через вказану користувачем кількість мілісекунд проміс має виконуватися (при fulfilled) або відхилятися (при rejected), залежно від обраної опції в радіокнопках. Значенням промісу, яке передається як аргумент у методи resolve/reject, має бути значення затримки в мілісекундах.
-
-Створений проміс треба опрацювати у відповідних для вдалого/невдалого виконання методах.
-
-Якщо проміс виконується вдало, виводь у консоль наступний рядок, де delay — це значення затримки виклику промісу в мілісекундах.
-
-
- */
-
 import { iziToast } from './libs';
+
 const form = document.querySelector('.js-form');
 
 form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
-  const delay = +form.elements.delay.value;
+  const delay = Number(form.elements.delay.value);
   const state = form.elements.state.value;
 
+  createPromise(delay, state)
+    .then(value =>
+      iziToast.success({
+        position: 'topRight',
+        message: `Fulfilled promise in ${value}ms`,
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        position: 'topRight',
+        message: `Rejected promise in ${error}ms`,
+      })
+    )
+    .finally(() => form.reset());
+}
+
+function createPromise(delay, state) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (state === 'fulfilled') {
@@ -26,19 +34,5 @@ function onFormSubmit(event) {
       }
     }, delay);
   });
-
-  promise
-    .then(value =>
-      iziToast.success({
-        position: 'topRight',
-        message: `✅ Fulfilled promise in ${value}ms`,
-      })
-    )
-    .catch(error =>
-      iziToast.error({
-        position: 'topRight',
-        message: `❌ Rejected promise in ${error}ms`,
-      })
-    )
-    .finally(() => form.reset());
+  return promise;
 }
