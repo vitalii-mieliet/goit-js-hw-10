b65d1504ca51296563bc9132266320b415056778
Will IM <vitalii.mieliet@gmail.com>
2025-05-07 22:34:20 +0300

    Do task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,44 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+/*
+Напиши скрипт, який після сабміту форми створює проміс. В середині колбека цього промісу через вказану користувачем кількість мілісекунд проміс має виконуватися (при fulfilled) або відхилятися (при rejected), залежно від обраної опції в радіокнопках. Значенням промісу, яке передається як аргумент у методи resolve/reject, має бути значення затримки в мілісекундах.
+
+Створений проміс треба опрацювати у відповідних для вдалого/невдалого виконання методах.
+
+Якщо проміс виконується вдало, виводь у консоль наступний рядок, де delay — це значення затримки виклику промісу в мілісекундах.
+
+
+ */
+
+import { iziToast } from './libs';
+const form = document.querySelector('.js-form');
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const delay = +form.elements.delay.value;
+  const state = form.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else if (state === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value =>
+      iziToast.success({
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${value}ms`,
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        position: 'topRight',
+        message: `❌ Rejected promise in ${error}ms`,
+      })
+    )
+    .finally(() => form.reset());
+}
